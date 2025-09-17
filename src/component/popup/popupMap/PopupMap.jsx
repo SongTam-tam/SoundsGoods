c87f9af2bc0db26e7d4cb3e2c312411cc37b166a
SongTam-tam <gacbam0315@gmail.com>
2025-09-17 12:45:01 +0900

    123

--- src/component/popup/popupMap/PopupMap.jsx
+++ src/component/popup/popupMap/PopupMap.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import popupData from '../../../assets/api/mapData'
 import './style.scss';
+import MapList from './mapList/MapList';
 const PopupMap = () => {
+    const [data,setData] = useState(popupData)
+    useEffect(() => {
+        // SDK가 로드됐는지 확인
+        if (window.kakao && window.kakao.maps) {
+          const container = document.getElementById('map');
+          const options = {
+            center: new window.kakao.maps.LatLng(37.5253019, 126.9255316),
+            level: 5,
+          };
+          new window.kakao.maps.Map(container, options);
+        }
+      }, []);
     return (
         <div className="popup_map">
             <div className="left_map">
@@ -9,8 +23,11 @@ const PopupMap = () => {
                         <img src="/images/icons/right.png" alt="" />
                         <strong>더 많은 팝업을 찾아보세요</strong>
                     </p>
+                    <MapList data={data}/>
                 </div>
-                <div className="map_pic"></div>
+                <div className="map_pic">
+                    <div id="map"></div>
+                </div>
             </div>
 
             <div className="right_pic"></div>
