973e3e88ad52f22516103cc1efb81232e74eeff4
SongTam-tam <gacbam0315@gmail.com>
2025-09-19 16:20:07 +0900

    123

--- src/component/main/main_visual/Main_visual.jsx
+++ src/component/main/main_visual/Main_visual.jsx
@@ -1,24 +1,42 @@
 import './style.scss';
 
+const vis = [
+    {
+        id: 1,
+        img: '/images/main/visual00.jpg',
+        right: 'Play',
+        left: 'It',
+        class: 'first',
+    },
+    {
+        id: 2,
+        img: '/images/main/visual02.jpg',
+        right: 'Feel',
+        left: 'It',
+        class: 'second',
+    },
+    {
+        id: 3,
+        img: '/images/main/visual03.jpg',
+        right: 'Love',
+        left: 'It',
+        class: 'third',
+    },
+];
+
 const Main_visual = () => {
     return (
         <section id="main-visual">
             <div className="visual_wrap">
-                <div className="visual first_visual">
-                    <strong>Play</strong>
-                    <img src="/images/main/visual00.jpg" alt="" />
-                    <strong>It</strong>
-                </div>
-                <div className="visual second_visual">
-                    <strong>Play</strong>
-                    <img src="/images/main/visual02.jpg" alt="" />
-                    <strong>It</strong>
-                </div>
-                <div className="visual third_visual">
-                    <strong>Play</strong>
-                    <img src="/images/main/visual03.jpg" alt="" />
-                    <strong>It</strong>
-                </div>
+                {vis.map((item) => (
+                    <div className={`visual ${item.class}_visual`} key={item.id}>
+                        <strong>{item.right}</strong>
+                        <div className="pic">
+                            <img src={item.img} alt="" />
+                        </div>
+                        <strong>{item.left}</strong>
+                    </div>
+                ))}
             </div>
             {/* visual_wrap */}
         </section>
