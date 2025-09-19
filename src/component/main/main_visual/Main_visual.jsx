c1503f36b2591e7d94f9c1f7c003071449d67034
SongTam-tam <gacbam0315@gmail.com>
2025-09-19 11:59:39 +0900

    비주얼 작업중

--- src/component/main/main_visual/Main_visual.jsx
+++ src/component/main/main_visual/Main_visual.jsx
@@ -1,7 +1,28 @@
 import './style.scss';
 
 const Main_visual = () => {
-    return <section id="main-visual"></section>;
+    return (
+        <section id="main-visual">
+            <div className="visual_wrap">
+                <div className="visual first_visual">
+                    <strong>Play</strong>
+                    <img src="/images/main/visual00.jpg" alt="" />
+                    <strong>It</strong>
+                </div>
+                <div className="visual second_visual">
+                    <strong>Play</strong>
+                    <img src="/images/main/visual02.jpg" alt="" />
+                    <strong>It</strong>
+                </div>
+                <div className="visual third_visual">
+                    <strong>Play</strong>
+                    <img src="/images/main/visual03.jpg" alt="" />
+                    <strong>It</strong>
+                </div>
+            </div>
+            {/* visual_wrap */}
+        </section>
+    );
 };
 
 export default Main_visual;
